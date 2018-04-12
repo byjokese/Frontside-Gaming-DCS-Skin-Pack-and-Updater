1157620d65207d488e4262cee0a3605144b5ca10
By-Jokese <byjokese@gmail.com>
2018-04-12 12:08:02 +0200

    Add project files.

---
+++ FSG_DCS_Skin_Installer_Updater/UserControl1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FSG_DCS_Skin_Installer_Updater
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(393, 72);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(83, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Install/Update";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(31, 43);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(445, 23);
+			this.progressBar1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(29, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(293, 20);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Frontside Gaming Skin Installer/Updater";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(31, 72);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(356, 96);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.button2.Location = new System.Drawing.Point(393, 101);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(83, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Close";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// UserControl1
+			// 
+			this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button1);
+			this.Name = "UserControl1";
+			this.Size = new System.Drawing.Size(513, 182);
+			this.Load += new System.EventHandler(this.UserControl1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.Button button2;
+	}
+}
